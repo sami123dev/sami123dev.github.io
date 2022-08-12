f3e4afe5e1d761469fdf5f30ebf7eff275e2733b
Samuel <74317410+sami123dev@users.noreply.github.com>
2022-08-12 01:38:08 -0400

    segunda subida

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,28 @@
+function disableScroll(){  
+    var x = window.scrollX;
+    var y = window.scrollY;
+    window.onscroll = function(){ window.scrollTo(x, y) };
+}
+
+function enableScroll(){  
+    window.onscroll = null;
+}
+
+scroll = true
+
 addEventListener("DOMContentLoaded", () => {
     const btn_menu = document.querySelector(".btn_menu")
     if (btn_menu) {
         btn_menu.addEventListener("click", () => {
             const menu_items = document.querySelector(".menu_items")
             menu_items.classList.toggle("mostrar")
+            if (scroll == true) {
+                disableScroll();
+                scroll = false;
+            }else if (scroll == false) {
+                scroll = true;
+                enableScroll();
+            }
               })
         }
 })
